2291f558cffff6eeb92148bdb8c126c3444cf0c4
TI GIIRO <ti.rafaelteles@gmail.com>
2022-11-03 19:28:09 -0300

    javascript_aluramidi

---
+++ main.js
@@ -0,0 +1,28 @@
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;
+    
+    console.log(idAudio);
+
+    tecla.onclick = function(){       /* ---Funcao anonima--- */
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
+
